9aa3dc67c7c52227753b2d8cd56e184d9beef80f
FaizanAsifB <faizanasif.dev@gmail.com>
2024-07-03 21:54:18 +0200

    replaced unsplash with pexels

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import 'dotenv/config'
+import { createClient } from 'pexels'
+
+const client = createClient(process.env.PEXELS_API_KEY)
 
 const weatherApp = {
-  apiKey: process.env.API_KEY,
+  weatherApiKey: process.env.WEATHER_API_KEY,
   async fetchCoords(city) {
     try {
       const response = await fetch(
-        `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${this.apiKey}`
+        `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${this.weatherApiKey}`
       )
       const data = await response.json()
       const { lat, lon } = data[0]
@@ -17,7 +20,7 @@ const weatherApp = {
   async fetchData(data) {
     try {
       const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${data[0]}&lon=${data[1]}&appid=${this.apiKey}&units=metric`
+        `https://api.openweathermap.org/data/2.5/weather?lat=${data[0]}&lon=${data[1]}&appid=${this.weatherApiKey}&units=metric`
       )
       return await response.json()
     } catch (error) {
@@ -29,7 +32,28 @@ const weatherApp = {
     searchBar.value = ''
     return name
   },
-  async displayWeather(data) {
+  async getBgImg(query) {
+    let imgData = await client.photos.search({ query, per_page: 1 })
+    if (imgData.photos.length === 0) {
+      imgData = await client.photos.search({
+        query: 'nature',
+        per_page: 1,
+      })
+    }
+
+    document
+      .querySelector('.attribution--link')
+      .setAttribute('href', imgData.photos[0].url)
+    document
+      .querySelector('.attribution--user')
+      .setAttribute('href', imgData.photos[0].photographer_url)
+    document.querySelector('.attribution--user').innerText =
+      imgData.photos[0].photographer
+
+    document.querySelector('.attribution').classList.remove('hidden')
+    return imgData.photos[0].src.original
+  },
+  async displayWeather(data, bgImg) {
     const { name } = data
     const { temp, humidity } = data.main
     const { description, icon } = data.weather[0]
@@ -43,7 +67,7 @@ const weatherApp = {
     document.querySelector('.description').innerText = `${description}`
     document.querySelector('.humidity').innerText = `Humidity: ${humidity}%`
     document.querySelector('.wind').innerText = `Wind Speed: ${speed}km/h`
-    document.body.style.backgroundImage = `url(https://source.unsplash.com/1600x900/?${name})`
+    document.body.style.backgroundImage = `url(${bgImg})`
     document.querySelector('.weather--container').classList.remove('loading')
   },
 }
@@ -56,8 +80,9 @@ const btnHandler = async function () {
   if (!city) return
   const coords = await weatherApp.fetchCoords(city)
   const data = await weatherApp.fetchData(coords)
+  const bgImg = await weatherApp.getBgImg(data.name)
 
-  weatherApp.displayWeather(data)
+  weatherApp.displayWeather(data, bgImg)
 }
 
 btn.addEventListener('click', btnHandler)
@@ -70,11 +95,9 @@ const currentCoords = async function (pos) {
   const { latitude, longitude } = pos.coords
   const coords = [latitude, longitude]
   const data = await weatherApp.fetchData(coords)
-  weatherApp.displayWeather(data)
+  const bgImg = await weatherApp.getBgImg(data.name)
+  weatherApp.displayWeather(data, bgImg)
 }
-
-// const getCurrentPos = navigator.geolocation.getCurrentPosition(currentCoords)
-// console.log(getCurrentPos)
 
 const getCurrentPos = function () {
   if (navigator.geolocation)
